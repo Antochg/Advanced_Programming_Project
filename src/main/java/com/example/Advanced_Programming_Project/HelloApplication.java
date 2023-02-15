72e9d2b7ccb4949a038c2df91034fd7d479021c8
gcao2405 <gaowei.cao2000@gmail.com>
2023-02-15 09:49:40 +0100

    Init Project

---
+++ src/main/java/com/example/Advanced_Programming_Project/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.Advanced_Programming_Project;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
