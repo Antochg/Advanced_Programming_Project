d1763f1f16bd26ef29e5feb99d757c0334ec2717
Antoine CHENG <antoine.cheng2000@gmail.com>
2023-02-22 17:00:24 +0100

    Changement script sql

--- src/main/java/com/example/Advanced_Programming_Project/LogInServlet.java
+++ src/main/java/com/example/Advanced_Programming_Project/LogInServlet.java
@@ -2,29 +2,30 @@ package com.example.Advanced_Programming_Project;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/login")
-public class LogInServlet {
+public class LogInServlet extends HttpServlet  {
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
 
-        String login = request.getParameter("login");
-        String password = request.getParameter("password");
-
-        HttpSession session = request.getSession(true);
-
-        session.setAttribute("login", login);
-        session.setAttribute("password", password);
-
-        try {
-            Class.forName("persistantdata.MediathequeData");
-        }
-        catch(Exception e) {
-            e.printStackTrace();
-        }
+//        String login = request.getParameter("login");
+//        String password = request.getParameter("password");
+//
+//        HttpSession session = request.getSession(true);
+//
+//        session.setAttribute("login", login);
+//        session.setAttribute("password", password);
+//
+//        try {
+//            Class.forName("persistantdata.MediathequeData");
+//        }
+//        catch(Exception e) {
+//            e.printStackTrace();
+//        }
 
 //        Utilisateur user = Mediatheque.getInstance().getUser(login, password);
 //
--- src/main/java/com/example/Advanced_Programming_Project/LogOutServlet.java
+++ src/main/java/com/example/Advanced_Programming_Project/LogOutServlet.java
@@ -9,11 +9,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-import java.io.IOException;
-
 @WebServlet("/logout")
 public class LogOutServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response ) {
         /* Récupération et destruction de la session en cours */
         HttpSession session = request.getSession();
 
