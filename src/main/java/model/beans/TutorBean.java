e4d8e1dfb26bf482b91a0c1c290f1b77bed98014
Paul Ancelet <paul21ancelet@orange.fr>
2023-03-11 15:34:55 +0100

    Correction login & Partage persistence.xml problème

--- src/main/java/model/beans/TutorBean.java
+++ src/main/java/model/beans/TutorBean.java
@@ -7,19 +7,22 @@ import jakarta.persistence.Persistence;
 import jakarta.persistence.Query;
 import model.entity.TutorEntity;
 
+import java.util.List;
+
 @Stateless
 public class TutorBean {
     EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
 
     EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-    public TutorBean() {}
-
     public TutorEntity getTutor(String login, String password) {
         Query query = entityManager.createQuery("SELECT t FROM TutorEntity t WHERE t.login = ?1 AND t.password = ?2")
                 .setParameter(1, login)
                 .setParameter(2, password);
-        TutorEntity tutor = (TutorEntity) query.getSingleResult();
-        return tutor;
+
+        List<TutorEntity> tutors = query.getResultList();
+        return tutors.size() == 1 ? tutors.get(0) : null;
+        //TutorEntity tutor = (TutorEntity) query.getSingleResult();
+        //return tutor;
     }
 }
